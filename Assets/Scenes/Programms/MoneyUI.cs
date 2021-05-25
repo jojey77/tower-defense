a2ca27ffbeb82e27177508604ebe69d8b89a3231
Joel Spanagel <joel.spanagel@web.de>
2021-05-25 17:49:54 +0200

    initial commit

---
+++ Assets/Scenes/Programms/MoneyUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyUI : MonoBehaviour
+{
+    public Text moneyText;
+
+    private void Update()
+    {
+        moneyText.text = "$" + PlayerStats.Money.ToString();
+    }
+
+}
+
